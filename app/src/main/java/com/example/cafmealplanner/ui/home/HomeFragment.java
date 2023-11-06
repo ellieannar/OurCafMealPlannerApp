bb05f5a644172ab6295f959b979877ab7ec38a42
ellieannar <ellieanna.ross@hotmail.com>
2023-11-05 16:45:17 -0800

    Fixing up the navigation - still throws error. Attempting to clean.

--- app/src/main/java/com/example/cafmealplanner/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cafmealplanner/ui/home/HomeFragment.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.example.cafmealplanner.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
