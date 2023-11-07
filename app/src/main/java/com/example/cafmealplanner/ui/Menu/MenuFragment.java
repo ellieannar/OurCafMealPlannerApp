ecca49f0712cbe540c1d8344b2e7869324651468
ellieannar <ellieanna.ross@hotmail.com>
2023-11-06 19:22:54 -0800

    button and arrow actions implemented

--- app/src/main/java/com/example/cafmealplanner/ui/Menu/MenuFragment.java
+++ app/src/main/java/com/example/cafmealplanner/ui/Menu/MenuFragment.java
@@ -1,7 +1,6 @@
 package com.example.cafmealplanner.ui.Menu;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -14,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.Calendar;
+
 import com.example.cafmealplanner.R;
 import com.example.cafmealplanner.databinding.FragmentMenuBinding;
 
@@ -21,41 +22,93 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
 
     private FragmentMenuBinding binding;
 
+    //Local Calendar for current (viewed) date
+    private Calendar cal = Calendar.getInstance();
+    //Current actual date
+    private Calendar now = Calendar.getInstance();
+
+
+
+    //Months indexed
+    private String monthOfYear[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
+
+    //Default on create view
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MenuViewModel menuViewModel =
                 new ViewModelProvider(this).get(MenuViewModel.class);
-
         binding = FragmentMenuBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-
         return root;
     }
 
     //function for when view is created
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        //forward day button and onClickListener
-        Button forwardDay = getView().findViewById(R.id.forwardDayButton);
-        forwardDay.setOnClickListener(this);
-
-        //back day button and onClickListener
+        //Back day and forward day buttons
         Button backDay = getView().findViewById(R.id.backDayButton);
+        Button forwardDay = getView().findViewById(R.id.forwardDayButton);
+
+        //forward and back day onClickListener
+        forwardDay.setOnClickListener(this);
         backDay.setOnClickListener(this);
+
+        //get current date to display
+        Calendar cal = Calendar.getInstance();
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int year = cal.get(Calendar.YEAR);
+
+        // Display initial date
+        TextView label = getView().findViewById(R.id.dateTextView);
+        label.setText(monthOfYear[month] + " " + day + ", " + year);
     }
+
 
     //click handler
     public void onClick(View v) {
         //Date text to be changed
         TextView label = getView().findViewById(R.id.dateTextView);
+        int month;
+        int day;
+        int year;
+
         //depending on the element clicked (v), perform corresponding action
         if (v == getView().findViewById(R.id.forwardDayButton)) {
-            label.setText("MOVE FORWARD");
-        }else if (v == getView().findViewById(R.id.backDayButton)) {
-            label.setText("MOVE BACK");
+            cal.add(Calendar.DAY_OF_YEAR, 1);
+            month = cal.get(Calendar.MONTH);
+            day = cal.get(Calendar.DAY_OF_MONTH);
+            year = cal.get(Calendar.YEAR);
+            label.setText(monthOfYear[month] + " " + day + ", " + year);
+        } else if (v == getView().findViewById(R.id.backDayButton)) {
+            //move back one day
+            cal.add(Calendar.DAY_OF_YEAR, -1);
+            //update month, day year
+            month = cal.get(Calendar.MONTH);
+            day = cal.get(Calendar.DAY_OF_MONTH);
+            year = cal.get(Calendar.YEAR);
+            //display
+            label.setText(monthOfYear[month] + " " + day + ", " + year);
         }
+
+
+        //check if either arrow should be hidden
+        hideArrow();
     }
 
+
+    //hide arrows so date cannot go out of bounds
+    private void hideArrow() {
+        if (cal.get(Calendar.DAY_OF_YEAR) < now.get(Calendar.DAY_OF_YEAR)) {
+            getView().findViewById(R.id.backDayButton).setVisibility(View.INVISIBLE);
+        } else if (cal.get(Calendar.DAY_OF_YEAR) > now.get(Calendar.DAY_OF_YEAR)+5) {
+            getView().findViewById(R.id.forwardDayButton).setVisibility(View.INVISIBLE);
+        } else {
+            getView().findViewById(R.id.backDayButton).setVisibility(View.VISIBLE);
+            getView().findViewById(R.id.forwardDayButton).setVisibility(View.VISIBLE);
+        }
+    }
 
     @Override
     public void onDestroyView() {
