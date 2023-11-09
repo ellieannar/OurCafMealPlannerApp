3ef2d4767bd09329380984e1991f27a4eff8be47
ellieannar <ellieanna.ross@hotmail.com>
2023-11-08 22:18:02 -0800

    working on tags

--- app/src/main/java/com/example/cafmealplanner/mealView.java
+++ app/src/main/java/com/example/cafmealplanner/mealView.java
@@ -2,20 +2,32 @@ package com.example.cafmealplanner;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+
+import java.util.ArrayList;
 
 
 public class mealView extends FrameLayout {
 
 
+    public enum TAG_TYPE {
+        VEGAN, GLUTEN_FREE, HUMANE, FARM_FRESH,
+        LOCALLY_CRAFTED
+    }
+
+
     private FrameLayout frameLayout;
     private TextView nameOfMealTextView;
-    private TextView tagsTextView;
+    private ArrayList<TextView> tagsTextViews;
+    private LinearLayout tagsLinearLayout;
 
 
     public mealView(@NonNull Context context) {
@@ -41,10 +53,8 @@ public class mealView extends FrameLayout {
         LayoutInflater.from(context).inflate(R.layout.meal_view, this, true);
         frameLayout = findViewById(R.id.mealViewFrameLayout);
         nameOfMealTextView = findViewById(R.id.foodTitle);
-        tagsTextView = findViewById(R.id.tag);
+        tagsLinearLayout = findViewById(R.id.tagsLinearLayout);
 
-        //set values
-        nameOfMealTextView.setText("Pizza");
 
 
         LayoutParams layoutParams = new LayoutParams(
@@ -60,6 +70,31 @@ public class mealView extends FrameLayout {
     public void setMealName(String name) {
         nameOfMealTextView = findViewById(R.id.foodTitle);
         nameOfMealTextView.setText(name);
+    }
+
+    public void addTag(TAG_TYPE tagType) {
+        TextView t = new TextView(getContext());
+        t.setWidth(80);
+        t.setHeight(80);
+        t.setGravity(Gravity.CENTER);
+        t.setTextColor(ResourcesCompat.getColor(getResources(), R.color.white, null));
+        t.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.rounded_corners, null));
+
+        switch (tagType){
+            case VEGAN:
+                t.setText("v");
+                t.setTextSize(20);
+                t.getBackground().setTint(ResourcesCompat.getColor(getResources(), R.color.green, null));
+                break;
+            case HUMANE:
+                t.setText("H");
+                t.setTextSize(20);
+                t.getBackground().setTint(ResourcesCompat.getColor(getResources(), R.color.lightBlue, null));
+                break;
+        }
+
+        tagsLinearLayout.addView(t);
+
     }
 
 
