06e1d93c55177c925c569e9f41a302b920180bdd
MW1129 <130323370+MW1129@users.noreply.github.com>
2023-11-10 09:43:27 -0800

    Basic layout of meal info page, without navigation

---
+++ app/src/main/java/com/example/cafmealplanner/ui/Schedule/MealInfo.java
@@ -0,0 +1,66 @@
+package com.example.cafmealplanner.ui.Schedule;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.cafmealplanner.R;
+import com.example.cafmealplanner.mealView;
+
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.widget.LinearLayout;
+
+import java.util.Vector;
+
+public class MealInfo extends Fragment {
+
+    public static MealInfo newInstance() {
+        return new MealInfo();
+    }
+
+    private MealInfoViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meal_info, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Create the button to return to the schedule page and set the on click listener
+        ImageButton backToSchedule = (ImageButton) getView().findViewById(R.id.backToSchedule);
+        backToSchedule.setOnClickListener((View.OnClickListener) this);
+
+        // Add some meal views
+
+        LinearLayout dinnerLinearLayout = (LinearLayout)getView().findViewById(R.id.dinnerLinearLayout);
+        LinearLayout dinnerItems = new LinearLayout(getContext());
+        dinnerItems.setOrientation(LinearLayout.VERTICAL);
+
+        Vector<mealView> dinnerMeals = new Vector<mealView>(5);
+        for (int i = 0; i < 5; i++) {
+            dinnerMeals.add(new mealView(getContext()));
+        }
+
+        for (int i = 0; i < dinnerMeals.size(); i++) {
+            dinnerMeals.get(i).setMealName("PASTA");
+            dinnerItems.addView(dinnerMeals.get(i));
+        }
+
+        dinnerLinearLayout.addView(dinnerItems);
+    }
+
+    public void onClick(View v) {
+        if (v == getView().findViewById(R.id.backToSchedule)) {
+            // Start the schedule activity if the button is clicked
+            Intent intent = new Intent(getContext(), ScheduleFragment.class);
+            startActivity(intent);
+        }
+    }
+
+}
--- app/src/main/java/com/example/cafmealplanner/ui/Schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/cafmealplanner/ui/Schedule/ScheduleFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.autofill.AutofillValue;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cafmealplanner.R;
 import com.example.cafmealplanner.databinding.FragmentScheduleBinding;
 
 public class ScheduleFragment extends Fragment {
@@ -28,6 +35,21 @@ public class ScheduleFragment extends Fragment {
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    /*public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Create the button to switch to the meal info page and set the on click listener
+        Button toMealInfo = (Button) getView().findViewById(R.id.goToMealInfo);
+        toMealInfo.setOnClickListener(this::onClick);
+    }
+
+    public void onClick(View v) {
+        if (v == getView().findViewById(R.id.goToMealInfo)) {
+            // If the button is clicked, change fragments
+            Fragment fragment = new MealInfo();
+            getParentFragmentManager().beginTransaction().replace(this, fragment).commit();
+        }
+    }*/
+
 
     @Override
     public void onDestroyView() {
