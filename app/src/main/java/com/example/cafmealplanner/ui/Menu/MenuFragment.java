dff9204e35b5c4dec795f50deebe613174c01670
ellieannar <ellieanna.ross@hotmail.com>
2023-11-12 16:52:15 -0800

    Allows navigation to more info about singular meal

--- app/src/main/java/com/example/cafmealplanner/ui/Menu/MenuFragment.java
+++ app/src/main/java/com/example/cafmealplanner/ui/Menu/MenuFragment.java
@@ -1,5 +1,9 @@
 package com.example.cafmealplanner.ui.Menu;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -53,6 +58,9 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
         container.removeAllViews();
 
 
+        //button clicks manager
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(getContext());
+        lbm.registerReceiver(receiver, new IntentFilter("filter_string"));
 
         return root;
     }
@@ -245,8 +253,28 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
     }
 
 
-    public void testingFunction() {
-        Log.d("IT WORKED", "testingFunction");
+    //button clicks
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                String str = intent.getStringExtra("mealName");
+                Log.d("TAG", "It was received " + str);
+                displayMealInfo(str);
+            }
+        }
+    };
+
+
+    private void displayMealInfo(String title) {
+        // Create new fragment and transaction
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        // Replace whatever is in the fragment_container view with this fragment
+        transaction.replace(R.id.nav_host_fragment_activity_main, FoodInfo.class, null);
+        // Commit the transaction
+        transaction.commit();
     }
 
 
--- app/src/main/java/com/example/cafmealplanner/ui/Schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/cafmealplanner/ui/Schedule/ScheduleFragment.java
@@ -1,5 +1,8 @@
 package com.example.cafmealplanner.ui.Schedule;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +21,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.example.cafmealplanner.R;
 import com.example.cafmealplanner.databinding.FragmentScheduleBinding;
@@ -41,13 +45,32 @@ public class ScheduleFragment extends Fragment implements View.OnClickListener{
         container.removeAllViews();
 
 
+        //button clicks manager
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(getContext());
+        lbm.registerReceiver(receiver, new IntentFilter("filter_string"));
+
         return root;
     }
+
+
+    //button clicks
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                String str = intent.getStringExtra("mealName");
+                Log.d("TAG", "It was received " + str);
+                displayMealInfo(str);
+            }
+        }
+    };
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         setupLinearLayouts();
         Button editButton = view.findViewById(R.id.edit_schedule);
         editButton.setOnClickListener(this);
+
+
 
     }
 
@@ -200,6 +223,17 @@ public class ScheduleFragment extends Fragment implements View.OnClickListener{
         }
     }
 
+
+    private void displayMealInfo(String title) {
+        // Create new fragment and transaction
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        // Replace whatever is in the fragment_container view with this fragment
+        transaction.replace(R.id.nav_host_fragment_activity_main, FoodInfo.class, null);
+        // Commit the transaction
+        transaction.commit();
+    }
 
 
 
